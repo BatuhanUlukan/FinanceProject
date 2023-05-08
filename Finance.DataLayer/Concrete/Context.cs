1f1b4b20f83569346d151e2787db80e40477f0ae
Batuhan Ulukan <batuhanulukan_55@protonmail.com>
2023-05-08 17:26:21 +0300

    Updated

---
+++ Finance.DataLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using Finance.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.DataLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=WIN-KMFO5JKEP0U;Database=FinanceDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+    }
+}
