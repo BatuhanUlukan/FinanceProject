41c25c9ab9f26ef7f0dcd2e271e3f8d8a5524438
Batuhan Ulukan <batuhanulukan_55@protonmail.com>
2023-05-12 14:56:26 +0300

    Update

---
+++ Finance.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,58 @@
+﻿using Finance.DtoLayer.Dtos.AppUserDtos;
+using Finance.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.UserName,
+                    Name = appUserRegisterDto.Name,
+                    Surname = appUserRegisterDto.SurName,
+                    Email = appUserRegisterDto.Email,
+                    City="aaaa",
+                    District="bbbb",
+                    ImageUrl="cccc"
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
+// En az 6 karakterden oluşacak
+// En az 1 küçük harf
+// En az 1 büyük harf
+// En az 1 sembol
+// En az 1 sayı içermeli
---
+++ Finance.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Finance.PresentationLayer.Models
+{
+	public class CustomIdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordToShort",
+				Description = $"Paralo en az {length} karakter olmalıdır."
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Lütfen en az 1 büyük harf giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "Lütfen en az 1 rakam giriniz."
+			};
+		}  		
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Lütfen en az 1 adet sembol giriniz."
+			};
+		}
+	}
+}
